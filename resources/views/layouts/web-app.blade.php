e810c070bcad8afabdcde39769d703bd07b8e8a3
Ram <59142683+RamSwami0021@users.noreply.github.com>
2024-05-17 16:03:40 +0530

    Update web-app.blade.php

--- resources/views/layouts/web-app.blade.php
+++ resources/views/layouts/web-app.blade.php
@@ -9,7 +9,7 @@
     <meta content="" name="description">
 
     <!-- Favicon -->
-    {{-- <link href="img/favicon.ico" rel="icon"> --}}
+    {{-- <link href="asset{{('img/favicon.ico)}}" rel="icon"> --}}
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -23,17 +23,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="{{ asset('/web-assets/lib/animate/animate.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/web-assets/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/web-assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/public/web-assets/lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/public/web-assets/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/public/web-assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css') }}" rel="stylesheet" />
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="{{ asset('/web-assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/public/web-assets/css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="{{ asset('/web-assets/css/style.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="sweet-modal/dist/min/jquery.sweet-modal.min.css" />
-    <script src="sweet-modal/dist/min/jquery.sweet-modal.min.js"></script>
+    <link href="{{ asset('/public/web-assets/css/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('sweet-modal/dist/min/jquery.sweet-modal.min.css)}}" />
+    <script src="{{asset('sweet-modal/dist/min/jquery.sweet-modal.min.js)}}"></script>
     <style>
         .fade-carousel .carousel-inner .carousel-item {
             opacity: 0;
@@ -145,17 +145,17 @@
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('/web-assets/lib/wow/wow.min.js') }}"></script>
-    <script src="{{ asset('/web-assets/lib/easing/easing.min.js') }}"></script>
-    <script src="{{ asset('/web-assets/lib/waypoints/waypoints.min.js') }}"></script>
-    <script src="{{ asset('/web-assets/lib/counterup/counterup.min.js') }}"></script>
-    <script src="{{ asset('/web-assets/lib/owlcarousel/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('/web-assets/lib/tempusdominus/js/moment.min.js') }}"></script>
-    <script src="{{ asset('/web-assets/lib/tempusdominus/js/moment-timezone.min.js') }}"></script>
-    <script src="{{ asset('/web-assets/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js') }}"></script>
+    <script src="{{ asset('/public/web-assets/lib/wow/wow.min.js') }}"></script>
+    <script src="{{ asset('/public/web-assets/lib/easing/easing.min.js') }}"></script>
+    <script src="{{ asset('/public/web-assets/lib/waypoints/waypoints.min.js') }}"></script>
+    <script src="{{ asset('/public/web-assets/lib/counterup/counterup.min.js') }}"></script>
+    <script src="{{ asset('/public/web-assets/lib/owlcarousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('/public/web-assets/lib/tempusdominus/js/moment.min.js') }}"></script>
+    <script src="{{ asset('/public/web-assets/lib/tempusdominus/js/moment-timezone.min.js') }}"></script>
+    <script src="{{ asset('/public/web-assets/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js') }}"></script>
 
     <!-- Template Javascript -->
-    <script src="{{ asset('/web-assets/js/main.js') }}"></script>
+    <script src="{{ asset('/public/web-assets/js/main.js') }}"></script>
 
     {{-- Sweetalert --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
