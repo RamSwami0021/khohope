5c33513476c044c970370f607a8aae3b39f0e08b
Ram <59142683+RamSwami0021@users.noreply.github.com>
2024-05-20 16:03:59 +0530

    Update web-app.blade.php

--- resources/views/layouts/web-app.blade.php
+++ resources/views/layouts/web-app.blade.php
@@ -23,15 +23,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="{{ asset('/public/public/web-assets/lib/animate/animate.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/public/public/web-assets/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/public/public/web-assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/public/web-assets/lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/public/web-assets/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/public/web-assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css') }}" rel="stylesheet" />
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="{{ asset('/public/public/web-assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/public/web-assets/css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="{{ asset('/public/public/web-assets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('/public/web-assets/css/style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="sweet-modal/dist/min/jquery.sweet-modal.min.css" />
     <script src="sweet-modal/dist/min/jquery.sweet-modal.min.js"></script>
     <style>
